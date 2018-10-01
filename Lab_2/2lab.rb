0b0f2ca2162bce7eacf48436ddf9b0cb9ebb141d
Ekaterina Nikiforova <ukroliki@outlook.com>
2018-10-01 20:03:41 +0300

    Add files via upload

---
+++ Lab_2/2lab.rb
@@ -0,0 +1,9 @@
+﻿require 'nokogiri'
+require 'open-uri'
+
+doc = Nokogiri::HTML(open('https://ru.wikipedia.org/wiki/%D0%9D%D0%BE%D0%B2%D0%BE%D1%81%D0%B8%D0%B1%D0%B8%D1%80%D1%81%D0%BA'))
+
+puts "Население:"
+doc.css('tr td span.no-wikidata span.nowrap', 'article td').each do |link|
+  puts link.content
+end
